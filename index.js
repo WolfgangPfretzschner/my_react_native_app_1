9f52a9bed103d74d60c82aef1888a9ca220290ec
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-10-22 10:19:16 -0400

    first component

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-/** @format */
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/componenets/header'
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const App = () => (
+   <Header />
+)
 
-AppRegistry.registerComponent(appName, () => App);
+
+
+AppRegistry.registerComponent("albums", () => App);
